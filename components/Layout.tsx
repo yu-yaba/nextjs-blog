6995d8d1e59df50ca1e3859371142d22f291eb80
yu-yaba <kawai.yuya3401@gmail.com>
2023-04-19 22:20:24 +0900

    first commit

---
+++ components/Layout.tsx
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+
+const name = "shin code";
+export const siteTitle = "Next.js blog";
+
+function Layout ({children, home}: {children: any; home: any }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <img className={`${utilStyles.borderCircle} ${styles.headerHomeImage}`} src="/images/profile.png"/>
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>            
+            <img className={`${utilStyles.borderCircle}`} src="/images/profile.png"/>
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div>
+          <Link href="/">← ホームへ戻る</Link>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Layout;
---
+++ pages/posts/firstPost.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Link from "next/link";
+
+
+export default function firstPost () {
+  return (
+    <div>
+      <Head>
+        <title>最初の投稿</title>
+      </Head>
+      <h1>最初の投稿</h1>
+      <Link href="/">ホームに戻る</Link>
+    </div>
+  );
+}
+
